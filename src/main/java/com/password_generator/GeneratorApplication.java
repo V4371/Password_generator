fe1e6358cc6d65b7a6d5755e8aade8926d7cddee
V4371 <vlad.kazakov05@gmail.com>
2024-12-15 01:33:12 +0300

    Add files via upload

---
+++ src/main/java/com/password_generator/GeneratorApplication.java
@@ -0,0 +1,33 @@
+package com.password_generator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+/**
+ * 
+ * Class of application, that extends javafx.application.Application class from JavaFX.
+ * It is responsible for window control
+ */
+public class GeneratorApplication extends Application {
+    /**
+     * The function that is responsible for actions when the application is launched
+     * @param stage The basis for creating a GUI in JavaFX.
+     *             It is a container into which all other interface components are placed.
+     * @throws IOException checked exceptions type that is required by fxmlLoader.load() method
+     */
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GeneratorApplication.class.getResource("generator-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 370, 240);
+        stage.setTitle("Генератор пароля");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+}
---
+++ src/main/java/com/password_generator/Launcher.java
@@ -0,0 +1,16 @@
+package com.password_generator;
+
+import javafx.application.Application;
+
+/**
+ * Class responsible for application launch
+ */
+public class Launcher {
+    /**
+     * This is the main function, it implements the application launch.
+     *
+     */
+    public static void main(String[] args) {
+        Application.launch(GeneratorApplication.class, args);
+    }
+}
